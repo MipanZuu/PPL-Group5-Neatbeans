58b41f434de9e7f6829861941e9ebca67d8a0d2b
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2022-12-14 23:50:51 +0700

    connected database Established

--- src/e/review/EReview.java
+++ src/e/review/EReview.java
@@ -32,6 +32,7 @@ public class EReview {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Connection Established!");
         new mainPage().setVisible(true);
     }
     
